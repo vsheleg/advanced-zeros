d34393d1276b88effda4abb162c8859039991438
vshlg <varyasheleg14@gmail.com>
2019-03-19 14:05:17 +0300

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let baseDecomposition = primeDecomposition(base)
+
+  let numDecomposition = {}
+
+  for(let key in baseDecomposition) {
+    numDecomposition[key] = 0;
+    let n = number;
+    let keyPower = key;
+    while(Math.floor(n / keyPower) > 0) {
+      numDecomposition[key] += Math.floor(n / keyPower);
+      keyPower *= key;
+    }
+  }
+
+  let normalizedDivisorsFrequencies = Object.entries(numDecomposition).map(([k, v]) => v / baseDecomposition[k])
+
+  return Math.floor(Math.min(...normalizedDivisorsFrequencies))
+}
+
+
+function primeDecomposition(number) {
+  let decomposition = {}
+
+  for(var i = 2; i <= number; ++i) {
+    if(number % i == 0) {
+      decomposition[i] = 0
+    } 
+
+    while(number % i == 0) {
+      decomposition[i] += 1
+      number /= i
+    }
+  }
+
+  return decomposition
 }
